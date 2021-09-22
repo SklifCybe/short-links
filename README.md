8c8f67f2410bbc2af2ed29887f372666c95faa78
SklifCybe <i.s.toaccept@gmail.com>
2021-09-22 22:04:13 +0300

    create create-page and update readme.md

--- README.md
+++ README.md
@@ -3,7 +3,19 @@
 ### Frontend stack:
 1) React
 2) React-Router-Dom
-3) Materialize-css
+3) Redux
+4) React-Redux
+5) Axios
+6) Scss
+7) Materialize-css
 
 ### Backend stack:
-1) Express
+1) Nodejs
+2) Express
+3) Express-Validator
+4) MongoDb
+5) Mongoose
+6) JsonWebToken
+7) BcryptJs
+8) DoteEnv
+9) ShortId
--- client/src/pages/CreatePage.jsx
+++ client/src/pages/CreatePage.jsx
@@ -1,5 +1,51 @@
 import React from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export const CreatePage = () => {
-  return <div>Create Page</div>;
+  const [link, setLink] = React.useState('');
+  const history = useHistory();
+  const { token } = useSelector(({ auth }) => auth);
+
+  const linkEnterHandler = async (event) => {
+    if (event.code === 'Enter') {
+      try {
+        const { data } = await axios.post(
+          '/api/link/generate',
+          { from: link },
+          {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          },
+        );
+        history.push(`/detail/${data.link._id}`);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    return;
+  };
+
+  const linkChangeHandler = (event) => {
+    setLink(event.target.value);
+  };
+
+  return (
+    <div className="row">
+      <div className="col s10 offset-s1" style={{ marginTop: '1rem' }}>
+        <div className="input-field">
+          <input
+            id="link"
+            type="text"
+            value={link}
+            onKeyPress={linkEnterHandler}
+            onChange={linkChangeHandler}
+          />
+          <label htmlFor="link">Link</label>
+        </div>
+      </div>
+    </div>
+  );
 };
